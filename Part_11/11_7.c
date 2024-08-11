7d396fa6c683c04326e7e08606a5e6cbd891fa17
leejw0709@gmail.com <leejw0709@gmail.com>
2024-08-11 23:18:12 +0900

    -

---
+++ Part_11/11_7.c
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(){
+    int input_1;
+    float input_2;
+    char input_3;
+    scanf("%d %f %c", &input_1, &input_2, &input_3);
+    printf("%d\n",input_1);
+    printf("%f\n",input_2);
+    printf("%c\n",input_3);
+}
