982480a2674ac316ae5bf6852a585d6f557d2f29
leejw0709@gmail.com <leejw0709@gmail.com>
2024-08-12 13:32:00 +0900

    -

---
+++ Part_15/15_5.c
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(){
+    int num;
+    scanf("%d", &num);
+
+    printf("%d ", num % 10);
+    num /= 10;
+    printf("%d ", num % 10);
+    num /= 10;
+    printf("%d ", num % 10);
+    num /= 10;
+    printf("%d ", num % 10);
+    num /= 10;
+    printf("%d ", num % 10);
+    num /= 10;
+}
---
+++ Part_16/16_5.c
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+    float num1;
+
+    scanf("%f", &num1);
+
+    int rst = num1;
+
+    printf("%d", rst);
+
+    return 0;
+}
